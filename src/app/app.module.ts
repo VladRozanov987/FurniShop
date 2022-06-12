38f6365acf858399a93c497af2a7760cea53838b
Vlad Rozanov <vvladrozanov@gmail.com>
2022-06-12 18:44:53 +0300

    for Sanya

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HeaderComponent } from './header/header.component';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { MainComponent } from './main/main.component';
+import { NewprodComponent } from './newprod/newprod.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    MainComponent,
+    NewprodComponent,  ],
   imports: [
     BrowserModule
   ],
---
+++ src/app/newprod/newprod.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newprod',
+  templateUrl: './newprod.component.html',
+  styleUrls: ['./newprod.component.css']
+})
+export class NewprodComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
