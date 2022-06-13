421693a1137fb4d670d4104770630c73ebce1120
Vlad Rozanov <vvladrozanov@gmail.com>
2022-06-13 18:09:41 +0300

    slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from '@angular/common';
 
 import { HeaderComponent } from './header/header.component';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { MainComponent } from './main/main.component';
-import { NewprodComponent } from './newprod/newprod.component';
+import { NewprodComponent } from './main/newprod/newprod.component';
 import { HeroComponent } from './main/hero/hero.component';
+import { SliderComponent } from './main/slider/slider.component';
+import { SwiperModule } from 'swiper/angular';
 
 @NgModule({
   declarations: [
@@ -15,11 +19,15 @@ import { HeroComponent } from './main/hero/hero.component';
     MenuComponent,
     MainComponent,
     NewprodComponent,
-    HeroComponent,  ],
+    HeroComponent,
+    SliderComponent,  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SwiperModule,
+    FormsModule,
+    CommonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
