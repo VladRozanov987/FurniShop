7d2b3c46d7111c9e5500f18b1159a46cc07db5a6
sashamanko <mankooleksandr748@gmail.com>
2022-06-14 03:19:05 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
+// Imports | Angular
+// __________________________________________________
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from "@angular/forms";
 import { CommonModule } from '@angular/common';
+
+// Imports | Swiper JS
+// __________________________________________________
 import { SwiperModule } from 'swiper/angular';
 
+// Imports | Components
+// __________________________________________________
 import { AppComponent } from './app.component';
+// Header
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './header/menu/menu.component';
+// Main
 import { MainComponent } from './main/main.component';
 import { NewprodComponent } from './main/newprod/newprod.component';
 import { HeroComponent } from './main/hero/hero.component';
@@ -14,6 +23,7 @@ import { CouterComponent } from './main/counter/counter.component';
 import { WedoComponent } from './main/wedo/wedo.component';
 import { SliderComponent } from './main/slider/slider.component';
 import { FormComponent } from './main/form/form.component';
+// Footer
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
